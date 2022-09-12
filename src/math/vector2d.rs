5e6a81502fde3c76c7715303ee1122c5d9d2ad67
Muhammad.M Norouzi <muhammadmnorouzi@gmail.com>
2022-09-12 18:07:36 +0430

    introduced dot magnitude normalize operations for Vector2D

--- src/math/vector2d.rs
+++ src/math/vector2d.rs
@@ -50,6 +50,14 @@ impl Mul<f32> for Vector2D {
     }
 }
 
+impl Mul<f32> for &Vector2D {
+    type Output = Vector2D;
+
+    fn mul(self, scalar: f32) -> Self::Output {
+        Vector2D::create(self.x * scalar, self.y * scalar)
+    }
+}
+
 impl MulAssign<f32> for Vector2D {
     fn mul_assign(&mut self, scalar: f32) {
         self.x *= scalar;
@@ -58,6 +66,15 @@ impl MulAssign<f32> for Vector2D {
 }
 
 impl Div<f32> for Vector2D {
+    type Output = Vector2D;
+
+    fn div(self, right: f32) -> Self::Output {
+        let right = 1.0f32 / right;
+        self * right
+    }
+}
+
+impl Div<f32> for &Vector2D {
     type Output = Vector2D;
 
     fn div(self, right: f32) -> Self::Output {
@@ -110,6 +127,18 @@ impl Default for Vector2D {
 impl Vector2D {
     pub fn create(x: f32, y: f32) -> Self {
         Self { x, y }
+    }
+
+    pub fn dot(&self, other: &Vector2D) -> f32 {
+        (self.x * other.x) + (self.y * other.y)
+    }
+
+    pub fn magnitude(&self) -> f32 {
+        return self.dot(self).sqrt();
+    }
+
+    pub fn normalize(&self) -> Vector2D {
+        self / self.magnitude()
     }
 }
 
@@ -210,6 +239,15 @@ mod tests {
     }
 
     #[test]
+    fn mul_scalar_borrowed_test() {
+        let v = &Vector2D::create(random_f32(), random_f32());
+        let scalar = random_f32();
+        let expected = Vector2D::create(v.x * scalar, v.y * scalar);
+
+        assert_eq!(expected, v * scalar);
+    }
+
+    #[test]
     fn mul_assign_scalar_test() {
         let x = random_f32();
         let y = random_f32();
@@ -233,6 +271,16 @@ mod tests {
     }
 
     #[test]
+    fn div_scalar_borrowed_test() {
+        let v = &Vector2D::create(random_f32(), random_f32());
+        let scalar = random_f32();
+        let scalar = 1.0f32 / scalar; //this is how we operate
+        let expected = Vector2D::create(v.x / scalar, v.y / scalar);
+
+        assert_eq!(expected, v / scalar);
+    }
+
+    #[test]
     fn div_assign_scalar_test() {
         let x = random_f32();
         let y = random_f32();
@@ -243,5 +291,30 @@ mod tests {
         v /= scalar;
 
         assert_eq!(expected, v);
+    }
+
+    #[test]
+    fn dot_test() {
+        let v1 = Vector2D::create(random_f32(), random_f32());
+        let v2 = Vector2D::create(random_f32(), random_f32());
+        let expected = (v1.x * v2.x) + (v1.y * v2.y);
+
+        assert_eq!(v1.dot(&v2), expected);
+    }
+
+    #[test]
+    fn magnitude_test() {
+        let v1 = &Vector2D::create(random_f32(), random_f32());
+        let expected = v1.dot(v1).sqrt();
+
+        assert_eq!(v1.magnitude(), expected);
+    }
+
+    #[test]
+    fn normalize_test() {
+        let v1 = &Vector2D::create(random_f32(), random_f32());
+        let expected = v1 / v1.magnitude();
+
+        assert_eq!(v1.normalize(), expected);
     }
 }
